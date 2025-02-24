29a3daf3b1ffd3aa3914563397806d13839a2927
1kko <me@1kko.com>
2025-02-24 14:05:28 +0900

    fix #2 - handles empty payloads and continue if decoding fails

--- feed_generator.py
+++ feed_generator.py
@@ -13,7 +13,7 @@ import email
 import email.header
 import hashlib
 import xml.etree.ElementTree as ET
-from datetime import datetime
+import datetime
 from pathlib import Path
 
 from feedgen.feed import FeedGenerator
@@ -27,6 +27,13 @@ from util import (
     extract_name_from_email,
     utf8_decoder,
 )
+
+
+def ensure_timezone(dt):
+    """Ensure datetime object has timezone info."""
+    if not dt.tzinfo:
+        return dt.replace(tzinfo=datetime.timezone.utc)
+    return dt
 
 
 def generate_rss(sender, messages):
@@ -54,57 +61,70 @@ def generate_rss(sender, messages):
             msg = email.message_from_bytes(mail_item.content)
             feed_entry = channel.add_entry()
 
+            # Process email subject
             title = email.header.make_header(email.header.decode_header(msg["subject"]))
             feed_entry.title(str(title))
 
+            # Extract the domain address from the sender email address
             feed_entry.link(href=f"https://{extract_domain_address(sender)}")
 
-            feed_entry.published(email.utils.parsedate_to_datetime(msg["date"]))
-
-            feed_entry.updated(email.utils.parsedate_to_datetime(msg["date"]))
+            # Process email published and updated date
+            dt = email.utils.parsedate_to_datetime(msg["date"])
+            dt = ensure_timezone(dt)
+            feed_entry.published(dt)
+            feed_entry.updated(dt)
 
             # Update the channel_data['pubDate'] to the latest email date
-            if (
-                channel_data.get("pubDate") is None
-                or channel_data.get("pubDate") < feed_entry.published()
-            ):
-                channel_data["pubDate"] = feed_entry.published()
+            if channel_data.get("pubDate") is None or channel_data.get("pubDate") < dt:
+                channel_data["pubDate"] = dt
 
+            # Process email sender information
             channel_name = email.utils.parseaddr(msg["from"])[0]
             if channel_name:
                 channel_data["name"] = channel_name
 
+            # Generate unique GUID
             unique_string = msg["subject"] + msg["date"] + msg["from"]
             guid = hashlib.md5(unique_string.encode()).hexdigest()
             feed_entry.id(guid)
 
+            # Process author information
             feed_entry.author({
                 "name": utf8_decoder(extract_name_from_email(msg["from"])),
                 "email": extract_email_address(sender),
             })
 
+            # Process email content
             content = ""
             html_content = None
             if msg.is_multipart():
                 for part in msg.walk():
-                    c_type = part.get_content_type()
-                    c_disp = str(part.get("Content-Disposition"))
-                    if "attachment" not in c_disp:
+                    content_type = part.get_content_type()
+                    content_disposition = str(part.get("Content-Disposition"))
+                    if "attachment" not in content_disposition:
                         charset = (
                             part.get_content_charset() or "utf-8"
                         )  # Default charset to utf-8
-                        if c_type == "text/html":
-                            html_content = cleanse_content(
-                                part.get_payload(decode=True).decode(
+                        payload = part.get_payload(decode=True)
+                        if payload:
+                            try:
+                                payload_decoded = payload.decode(
                                     charset, errors="ignore"
                                 )
-                            )
-                        elif c_type == "text/plain" and html_content is None:
-                            content = cleanse_content(
-                                part.get_payload(decode=True).decode(
-                                    charset, errors="ignore"
-                                )
-                            )
+                                if content_type == "text/html":
+                                    html_content = cleanse_content(payload_decoded)
+                                elif (
+                                    content_type == "text/plain"
+                                    and html_content is None
+                                ):
+                                    content = cleanse_content(
+                                        part.get_payload(decode=True).decode(
+                                            charset, errors="ignore"
+                                        )
+                                    )
+                            except Exception as e:
+                                logging.error(f"Failed to decode payload: {e}")
+                                continue
             else:
                 charset = msg.get_content_charset() or "utf-8"
                 if msg.get_content_type() == "text/html":
@@ -116,13 +136,16 @@ def generate_rss(sender, messages):
                         msg.get_payload(decode=True).decode(charset, errors="ignore")
                     )
 
+            # Set entry description
             # Prefer HTML content if available
             feed_entry.description(
                 html_content if html_content is not None else content
             )
 
+        # Finalize channel metadata
         channel.title(utf8_decoder(channel_data.get("name")))
-        channel.pubDate(channel_data.get("pubDate"))
+        if channel_data.get("pubDate"):
+            channel.pubDate(ensure_timezone(channel_data.get("pubDate")))
 
         logging.info(f"Generated RSS feed for {sender}.")
         return channel.rss_str(pretty=True).decode("utf-8")
@@ -205,7 +228,7 @@ def create_opml_from_files(
 
     # Add date elements
     # RFC 2822 Format (Used in many OPML examples)
-    now = datetime.now().strftime("%a, %d %b %Y %H:%M:%S %z")
+    now = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S %z")
     ET.SubElement(head, "dateCreated").text = now
     ET.SubElement(head, "dateModified").text = now
 
@@ -275,9 +298,13 @@ def main():
     for sender in db.get_senders():
         messages = db.get_email(sender)
         logging.info(f"{sender} found entries={messages.count()}")
-        rss_feed = generate_rss(sender, messages)
-        feed_file_path = save_feed(sender, rss_feed, save_path=data_feed_dir)
-        rss_files.append(feed_file_path)
+        try:
+            rss_feed = generate_rss(sender, messages)
+            feed_file_path = save_feed(sender, rss_feed, save_path=data_feed_dir)
+            rss_files.append(feed_file_path)
+        except Exception as e:
+            logging.error(f"Skipping {sender} due to error: {e}")
+            continue
 
     # aggregate all the feeds into a single OPML file
     create_opml_from_files(rss_files, save_path=data_feed_dir)
